3e4d7942fccc4c8b021a7e8fb3af703d9f4719ff
Akshay Gokani <goka0004@algonquinlive.com>
2023-05-28 15:19:50 -0400

    add functions and button

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,11 +63,11 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_food_load_list()
     streamlit.dataframe(my_data_rows)
 
-
+#allow end user to add fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
             my_cur.execute("insert into fruit_load list valuse ('from streamlit')")
-            return f'thanks for adding {new_fruit}'
+            return f'thanks for adding' + new_fruit
 
 add_my_fruit =  streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
