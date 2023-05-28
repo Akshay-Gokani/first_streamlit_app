168be9505f6380f509c13d42f91d6c2c87306e82
Akshay Gokani <goka0004@algonquinlive.com>
2023-05-28 15:24:55 -0400

    add functions and button

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
         # normalize json data - takes out "" puts them all on same line
         fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
         return fruityvice_normalized
@@ -45,9 +45,6 @@ try:
         streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
-
-#dont run anything past here while we trouble
-
 
 streamlit.header("The fruit load list contains.")
 # snowflake related functions
@@ -66,8 +63,8 @@ if streamlit.button('Get Fruit Load List'):
 #allow end user to add fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-            my_cur.execute("insert into fruit_load list valuse ('from streamlit')")
-            return f'thanks for adding' + new_fruit
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')") 
+        return 'thanks for adding' + new_fruit
 
 add_my_fruit =  streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
